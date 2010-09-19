ed31a410efc4b990d0e5b16dc109b6e66f54d36e
Evan Grim <evan@mirgnave.com>
2010-09-19 15:45:32 -0500

    Improve with better comments/constants/algorithms
    Renamed symbolic constants to be shorter and better
    Improved/added comments and comment formatting
    Fixed calculations to better handle corner cases

--- assignment_2.c
+++ assignment_2.c
@@ -9,22 +9,22 @@
 #define TRUE 1
 #define FALSE 0
 
-#define DELTA_FROM_CDT_TO_SAST 7
+#define CDT_TO_SAST_OFFSET 7
 
-#define FACTOR_USD_PER_ZAR 0.137011
-#define FACTOR_ZAR_PER_USD 7.29869
-#define FACTOR_DOLLARS_PER_CENT .01
-#define FACTOR_CENTS_PER_DOLLAR 100
+#define USD_PER_ZAR 0.137011
+#define ZAR_PER_USD 7.29869
+#define DOLLARS_PER_CENT .01
+#define CENTS_PER_DOLLAR_INT 100
 
-#define FACTOR_F_TO_C (5.0/9.0)
-#define FACTOR_C_TO_F (9.0/5.0)
-#define DELTA_F_TO_C 32
+#define C_TO_F_SCALAR (9.0/5.0)
+#define F_TO_C_SCALAR (5.0/9.0)
+#define F_TO_C_OFFSET 32
 
-#define FACTOR_OZ_PER_L 33.8140227
-#define FACTOR_OZ_PER_GALLON 128
+#define OZ_PER_L 33.8140227
+#define OZ_PER_GALLON_INT 128
 
-#define FACTOR_MILES_PER_KM .6213712
-#define FACTOR_KM_PER_MILE 1.609355
+#define MILES_PER_KM .6213712
+#define KM_PER_MILE 1.609355
 
 typedef unsigned char Bool;
 
@@ -52,8 +52,8 @@ int main()
 		Bool different_day;
 
 		// Money variable(s)
-		int dollars;
-		float usd, zar, cents;
+		int dollars, cents;
+		float usd, zar;
 
 		// Temperature variable(s)
 		float fahrenheit, celsius;
@@ -89,12 +89,14 @@ int main()
 		switch(menu_choice)
 		{
 			case 1: // Time: CDT -> SAST
-				// Prompt for input
+				/* Prompt for input */
 				printf("Enter an Austin time to be converted (in hours and minutes): ");
 				scanf("%d %d", &hour_input, &minute);
 
-				// Perform conversion
-				hour_output = hour_input + DELTA_FROM_CDT_TO_SAST;
+				/* Perform conversion */
+				hour_output = hour_input + CDT_TO_SAST_OFFSET;
+
+				// Correct hour if rolled over to next day
 				if (hour_output > 23)
 				{
 					hour_output -= 24;
@@ -103,9 +105,11 @@ int main()
 				else
 					different_day = FALSE;
 
-				// Print output
+				/* Print output */
 				printf("The equivalent time in South Africa is: %.2d %.2d",
 						hour_output, minute);
+
+				// Add day change notice if appropriate
 				if (different_day == TRUE)
 				{
 					printf(" (the day after)");
@@ -115,12 +119,14 @@ int main()
 				break;
 
 			case 2: // Time: SAST -> CDT
-				// Prompt for input
+				/* Prompt for input */
 				printf("Enter a South African time to be converted (in hours and minutes): ");
 				scanf("%d %d", &hour_input, &minute);
 
-				// Perform conversion
-				hour_output = hour_input - DELTA_FROM_CDT_TO_SAST;
+				/* Perform conversion */
+				hour_output = hour_input - CDT_TO_SAST_OFFSET;
+
+				// Correct hour if rolled back to next day
 				if (hour_output < 0)
 				{
 					hour_output += 24;
@@ -131,9 +137,11 @@ int main()
 					different_day = FALSE;
 				}
 
-				// Print output
+				/* Print output */
 				printf("The equivalent time in Austin is: %.2d %.2d",
 						hour_output, minute);
+
+				// Add day change notice if appropriate
 				if (different_day == TRUE)
 				{
 					printf(" (the day before)");
@@ -143,102 +151,119 @@ int main()
 				break;
 
 			case 3: // Money: USD -> ZAR
-				// Prompt for input
+				/* Prompt for input */
 				printf("Enter a US currency amount to be converted (in dollars and cents): ");
-				scanf("%d %f", &dollars, &cents);
+				scanf("%d %d", &dollars, &cents);
 
-				// Perform conversion
-				usd = dollars + cents*FACTOR_DOLLARS_PER_CENT;
-				zar = usd * FACTOR_ZAR_PER_USD;
+				/* Perform conversion */
+				usd = dollars + cents*DOLLARS_PER_CENT;
+				zar = usd * ZAR_PER_USD;
 
-				// Print output (let printf do the rounding)
+				/* Print output (let printf do the rounding) */
 				printf("The equivalent currency amount in ZAR is: %.2f\n", zar);
 				break;
 
 			case 4: // Money: ZAR -> USD
-				// Prompt for input
+				/* Prompt for input */
 				printf("Enter a South African currency amount to be converted (in ZAR): ");
 				scanf("%f", &zar);
 
-				// Perform conversion
-				usd = zar * FACTOR_USD_PER_ZAR;
-				dollars = (int) usd;
-				cents = (usd - dollars) * FACTOR_CENTS_PER_DOLLAR;
+				/* Perform conversion */
 
-				// Print output (let printf do the rounding)
-				printf("The equivalent currency amount in USD (dollars and cents) is: %d %.0f\n", dollars, cents);
+				// Convert ZAR to cents (add .5  and truncate to round)
+				cents = (int)(zar * USD_PER_ZAR * CENTS_PER_DOLLAR_INT + .5);
+
+				// Pull out the dollar amount and adjust value of cents
+				dollars = cents / CENTS_PER_DOLLAR_INT;
+				cents = cents % CENTS_PER_DOLLAR_INT;
+
+				/* Print output */
+				printf("The equivalent currency amount in USD (dollars and cents) is: %.2d %.2d\n", dollars, cents);
 				break;
 
 			case 5: // Temperature: F -> C
-				// Prompt for input
+				/* Prompt for input */
 				printf("Enter a US temperature to be converted (in  Fahrenheit): ");
 				scanf("%f", &fahrenheit);
 
-				// Perform conversion
-				celsius = FACTOR_F_TO_C * (fahrenheit - DELTA_F_TO_C);
+				/* Perform conversion */
+				celsius = F_TO_C_SCALAR * (fahrenheit - F_TO_C_OFFSET);
 
-				// Print output (rounding to 2 decimal place digits not required by assignment)
+				/* Print output (rounding to 2 decimal place digits not required by assignment) */
 				printf("The equivalent temperature in Celsius is: %.2f\n", celsius);
 				break;
 
 			case 6: // Temperature: C -> F
-				// Prompt for input
+				/* Prompt for input */
 				printf("Enter a South African temperature to be converted (in Celsius): ");
 				scanf("%f", &celsius);
 
-				// Perform conversion
-				fahrenheit = (FACTOR_C_TO_F * celsius) + DELTA_F_TO_C;
+				/* Perform conversion */
+				fahrenheit = (C_TO_F_SCALAR * celsius) + F_TO_C_OFFSET;
 
-				// Print output (let printf do the rounding)
+				/* Print output (let printf do the rounding) */
 				printf("The equivalent temperature in Fahrenheit (rounded) is: %.0f\n", fahrenheit);
 				break;
 
 			case 7: // Volume: L -> gal/fl.oz
-				// Prompt for input
+				/* Prompt for input */
 				printf("Enter a South African volume to be converted (in liters): ");
 				scanf("%f", &liters);
 
-				// Perform conversion
-				gallons = (int) (liters * FACTOR_OZ_PER_L / FACTOR_OZ_PER_GALLON);
-				ounces = liters * FACTOR_OZ_PER_L - gallons * FACTOR_OZ_PER_GALLON;
+				/* Perform conversion */
 
-				// Print output (let printf do the rounding)
-				printf("The equivalent volume in gallons and fluid ounces is: %d %.3f\n", gallons, ounces);
+				// Convert L to fluid ounces
+				ounces = liters * OZ_PER_L;
+
+				// Round to 3 digits by:
+				//	* mult by 1000
+				//  * add .5
+				//  * truncate
+				//  * dived by 1000
+				ounces = ((int)(ounces * 1000 + .5))/1000;
+
+				// Pull out number of gallons and adjust value of ounces
+				gallons = ((int) ounces) / OZ_PER_GALLON_INT;
+				ounces = ounces - gallons * OZ_PER_GALLON_INT;
+
+
+				/* Print output */
+				printf("The equivalent volume in gallons and fluid ounces is: %d %f\n", gallons, ounces);
 				break;
 
 			case 8: // Volume: gal/fl.oz -> L
-				// Prompt for input
+				/* Prompt for input */
 				printf("Enter an Austin volume to be converted (in gallons and fluid ounces): ");
 				scanf("%d %f", &gallons, &ounces);
 
-				// Perform conversion
-				liters = (gallons * FACTOR_OZ_PER_GALLON + ounces) / FACTOR_OZ_PER_L;
+				/* Perform conversion */
+				liters = (gallons * OZ_PER_GALLON_INT + ounces) / OZ_PER_L;
 
-				// Print output (rounding to 3 decimal place digits not required by assignment)
+				/* Print output (rounding to 3 decimal place digits not required by assignment) */
 				printf("The equivalent volume in liters is: %.3f\n", liters);
 				break;
 
 			case 9: // Distance: km -> miles
-				// Prompt for input
+				/* Prompt for input */
 				printf("Enter a South African distance to be converted (in km): ");
 				scanf("%f", &km);
 
-				// Perform conversion
-				miles = km * FACTOR_MILES_PER_KM;
+				/* Perform conversion */
+				miles = km * MILES_PER_KM;
 
-				// Print output (rounding to 3 decimal place digits not required by assignment)
+				/* Print output (rounding to 3 decimal place digits not required by assignment) */
 				printf("The equivalent distance in miles is: %.3f\n", miles);
 				break;
 
 			case 10:// Distance: miles -> km
-				// Prompt for input
+				/* Prompt for input */
 				printf("Enter an Austin distance to be converted (in miles): ");
 				scanf("%f", &miles);
 
-				// Perform conversion
-				km = miles * FACTOR_KM_PER_MILE;
+				/* Perform conversion */
+				km = miles * KM_PER_MILE;
 
-				// Print output (rounding to 3 decimal place digits not required by assignment)
+				/* Print output (rounding to 3 decimal place digits not required by assignment) */
 				printf("The equivalent distance in km is: %.3f\n", km);
 				break;
 
@@ -254,6 +279,9 @@ int main()
 		// Flush any unread input by reading until a newline character is seen
 		while (getchar() != '\n');
 	}
+
+	// Pause program before exiting by using getchar()
+	getchar();
 
 	return 0;
 }
